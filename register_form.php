3e5706e3b25f13644393ff5045beefdb560ee808
IT22589804 <it22589804@my.sliit.lk>
2023-06-05 23:09:40 +0530

    new fields add to the form

--- register_form.php
+++ register_form.php
@@ -59,10 +59,17 @@ if(isset($_POST['submit'])){
          };
       };
       ?>
-      <input type="text" name="name" required placeholder="enter your name">
+      <input type="text" name="fname" required placeholder="enter your first name">
+      <input type="text" name="lname" required placeholder="enter your last name">
       <input type="email" name="email" required placeholder="enter your email">
+      <input type="tel" name="pnumber" pattern="[0-9]{3}-[0-9]{3} [0-9]{4}" required placeholder="enter your mobile number">
+      <input type="text" name="nic" required placeholder="enter your NIC">
+      <input type="date" name="birthday" required placeholder="enter your birthday">
+      <input type="radio" name="gender" value="Male"><label>Male</label>
+      <input type="radio" name="gender" value="Female"><label>Female</label><br>
       <input type="password" name="password" required placeholder="enter your password">
       <input type="password" name="cpassword" required placeholder="confirm your password">
+      <input type="checkbox" name="terms" value="true"><label>accept terms and conditions</label>
       <select name="user_type">
          <option value="user">user</option>
          <option value="admin">admin</option>
