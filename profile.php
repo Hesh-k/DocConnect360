4a1d9c6a3d51799a59029ce279175c55353f8876
IT22589804 <it22589804@my.sliit.lk>
2023-06-10 12:54:04 +0530

    user profile edited

--- profile.php
+++ profile.php
@@ -4,8 +4,32 @@
 
 session_start();
 
-if(!isset($_SESSION['user_name'])){
-   header('location:login_form.php');
+if (!isset($_SESSION['user_name'])) {
+    header('location:login_form.php');
+}
+
+// Fetch user details from the database
+$email = $_SESSION['user_email'];
+$sql = "SELECT * FROM user_form WHERE email = '$email'";
+$result = $conn->query($sql);
+
+// Check if user exists
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $fname = $row['fname'];
+    $lname = $row['lname'];
+    $birthday = $row['birthday'];
+    $gender = $row['gender'];
+    $email = $row['email'];
+    $pnumber = $row['pnumber'];
+} else {
+    // Handle user not found scenario
+    $fname = '';
+    $lname = '';
+    $birthday = '';
+    $gender = '';
+    $email = '';
+    $pnumber = '';
 }
 
 ?>
@@ -21,6 +45,8 @@ if(!isset($_SESSION['user_name'])){
     <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style3.css">
+
 </head>
 <body>
     <div class="header-container">
@@ -44,18 +70,42 @@ if(!isset($_SESSION['user_name'])){
             <div class="login-container">
                 <a href="logout.php" id="login">Logout</a>
             </div>
-        </div>
+    </div>
 
-        <div class="container">
-            <div class="content">
-            <h3>hi, <span>user</span></h3>
-            <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
+    <div class="container">
+        <div class="content">
+            <h3>Hi, <span><?php echo $_SESSION['user_name'] ?></span></h3>
+            <h1>Welcome to Your Profile</h1>
+            <div class="profile-details">
+                <h2>User Information</h2>
+                <table>
+                    <tr>
+                        <th>Full Name:</th>
+                        <td><?php echo $fname . ' ' . $lname; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Date of Birth:</th>
+                        <td><?php echo $birthday; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Gender:</th>
+                        <td><?php echo $gender; ?></td>
+                    </tr>
+                    <tr>
+                        <th>E-mail:</th>
+                        <td><?php echo $email; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Phone Number:</th>
+                        <td><?php echo $pnumber; ?></td>
+                    </tr>
+                </table>
+            </div>
             <a href="editprofile.php" class="btn">Edit Profile</a>
             <a href="deleteprofile.php" class="btn">Delete Profile</a>
-            
-         </div>
+        </div>
+    </div>
 
-</div>
     <div class="footer-container">
         <div class="footer-info">
             <p>A VirtualVitals Company<br>Copyright (C) 2023 by VirtualVitals</p>
@@ -67,7 +117,7 @@ if(!isset($_SESSION['user_name'])){
             <a href="#">Our Mission</a>
         </div>
         <div class="social-buttons">
-            <p>Connect us on</p>
+            <p>Connect with us on</p>
             <img src="assets/social.png" class="social">
             <p>Download our free app</p>
             <img src="assets/apps.png" class="apps">
