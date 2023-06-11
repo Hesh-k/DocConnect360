867987e2523b62b5f710ecff48911b35197bc263
IT22589804 <it22589804@my.sliit.lk>
2023-06-11 18:55:48 +0530

    VALIDATION

--- appointmentData.php
+++ appointmentData.php
@@ -1,11 +1,18 @@
 <?php
 
-include_once 'config.php';
+@include 'config.php';
+
 
 session_start();
 
 $useremail = $_SESSION['user_email'];
 
+$sql = "SELECT id FROM user_form WHERE email = '$useremail'";
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $user_id = $row['id'];
 
 
  $name = $_POST['DoctorName'];
@@ -13,10 +20,12 @@ $useremail = $_SESSION['user_email'];
  $Number = $_POST['Number'];
 
 
-$sql = "INSERT INTO appointments (doctorName, day, number) VALUES('$name','$date','$Number');";
+$sql = "INSERT INTO appointments (user_id, doctorName, day, number) VALUES('$user_id','$name','$date','$Number');";
 
 $result = mysqli_query($conn,$sql);
 
 header("Location:bookappointment.php");
+
+}
 
 ?>
--- appointments.php
+++ appointments.php
@@ -21,9 +21,20 @@
                 <th>Actions</th>
             </tr>
             <?php
+
+                ini_set('display_errors', 0);
+                error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+                session_start();
                 include_once "config.php";
-                $sql = "SELECT * FROM appointments";
+
+                $userid = $_SESSION['user_id'];
+
+                $sql = "SELECT * FROM appointments WHERE user_id = '$userid'";
                 $result = $conn->query($sql);
+
+
+        
 
                 if (!$result) {
                     die("Invalid query!");
@@ -40,8 +51,8 @@
                         <a class='btn btn-danger' href='deleteAppointment.php?appointment_id=$row[appointment_id]'>Cancel</a>
                     </td>
                 </tr>
-                ";
-                }
+                ";}
+                
             ?>
         </table>
     </div>
