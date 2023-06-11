ff3c2302a79a06764435cdda4b6384c992c2afd2
LanaXD <128351799+IT22589668@users.noreply.github.com>
2023-06-11 19:35:30 +0530

    booking

--- bookAppointment.php
+++ bookAppointment.php
@@ -7,31 +7,53 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
 </head>
 <body>
-    <div class="form">
-        <form  method ="POST" action="appointmentData.php">
-            <h2>Book Appointment</h2>
-            <button id="back-btn"><i class="fa-sharp fa-solid fa-arrow-left"></i><a href="appointments.php">&nbsp;View Appointments</a></button>
-            <div class="input-container ic2">
-            <input type="text" class="input" name = "DoctorName" placeholder="Doctor's Name" required>
+<div class="form">
+    <form method="POST" action="appointmentData.php">
+        <h2>Book Appointment</h2>
+        <button id="back-btn"><i class="fa-sharp fa-solid fa-arrow-left"></i><a href="appointments.php">&nbsp;View Appointments</a></button>
+        <div class="input-container ic2">
+            <?php
+            include_once 'config.php';
+
+            $sql = "SELECT doctor_id, fname, lname FROM doctor";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo '<select name="DoctorName" class="input">';
+                while ($row = $result->fetch_assoc()) {
+
+                    $fname = $row['fname'];
+                    $lname = $row['lname'];
+
+                    echo "<option>$fname $lname</option>";
+                }
+                echo '</select>';
+            } else {
+                echo 'No data available.';
+            }
+
+            $conn->close();
+            ?>
             <div class="cut"></div>
-            </div>
-            <div class="input-container ic2">
+        </div>
+        <div class="input-container ic2">
             <input type="date" class="input" name="date" id="date" required>
             <div class="cut"></div>
-            </div>
-            <div class="input-container ic2">
-            <input type="text" class="input" name = "Number" placeholder="Phone Number" required>
-            <div class="cut cut-short"></div>    
-            </div >
-            <div>
-            <input type="submit" class="submit" value="Confirm Appoinment" onclick="showAlert()">
-            </div>    
-        </form>
-    </div>
-    <script type="text/javascript">
-        function showAlert() {
-            alert("You have Sucssessfuly Book An Appointment!");
-        }
-    </script>
+        </div>
+        <div class="input-container ic2">
+            <input type="text" class="input" name="Number" placeholder="Phone Number" required>
+            <div class="cut cut-short"></div>
+        </div>
+        <div>
+            <input type="submit" class="submit" value="Confirm Appointment" onclick="showAlert()">
+        </div>
+    </form>
+</div>
+<script type="text/javascript">
+    function showAlert() {
+        alert("You have successfully booked an appointment!");
+    }
+</script>
+
 </body>
 </html>
